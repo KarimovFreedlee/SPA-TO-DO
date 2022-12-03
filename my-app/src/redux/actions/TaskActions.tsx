7478481c0ef31de3c952aa18dedb797290b7df1f
KarimovFreedlee <45467077+KarimovFreedlee@users.noreply.github.com>
2022-12-03 23:43:29 +0500

    Update TaskActions.tsx

--- my-app/src/redux/actions/TaskActions.tsx
+++ my-app/src/redux/actions/TaskActions.tsx
@@ -1,7 +1,7 @@
 import { AnyAction } from "redux"
 import { ITask } from "../../components/taskScene/Task"
 import { ITaskColumn } from "../../components/taskScene/Tasks"
-import { IAction, SET_ALL_TASKS } from "../reducers/MainReducer"
+import { IAction, SET_ALL_COLUMNS, SET_ALL_TASKS } from "../reducers/MainReducer"
 
 export const setTasks = (tasks: ITask[]) => {
     const newAction: IAction = {
@@ -13,7 +13,7 @@ export const setTasks = (tasks: ITask[]) => {
 
 export const setColumns = (columns: ITaskColumn[]) => {
     const newAction: IAction = {
-        type: SET_ALL_TASKS,
+        type: SET_ALL_COLUMNS,
         payload: columns
     }
     return newAction
